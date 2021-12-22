e99a88a2136586f28aad6a86c02411c9584dc051
fr4p3rzz <francesco95peruzzi@gmail.com>
2021-12-22 11:44:45 +0100

    updated style and js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-/** The bois' annual income */
+/** The bois' annual income in dollars */
 let elonMusk = 12648672874;
 let bernardArnault = 94100000000;
 let billGates = 3796000000;
@@ -8,3 +8,52 @@ let markZuckerberg = 3000000000;
 let warrenBuffet = 13500000000;
 let jeffBezos = 73000000000;
 let jkRowling = 60000000;
+
+/** change from dollar */
+let euro = 0.89;
+let yen = 114.31;
+let ruble = 73.78;
+let rupee = 75.50;
+
+/** other stuff */
+let commaValue = 2
+
+function incomePermonth(income)
+{
+    return (income / 12).toFixed(commaValue);
+}
+
+function incomePerDay(income)
+{
+    return (income / 365).toFixed(commaValue);
+}
+
+function incomePerMinute(income)
+{
+    return (income / 525600).toFixed(commaValue);
+}
+
+function incomePerSecond(income)
+{
+    return (income / 31536000).toFixed(commaValue);
+}
+
+function toEur(income)
+{
+    return (income * euro).toFixed(commaValue);
+}
+
+function toYen(income)
+{
+    return (income * yen).toFixed(commaValue);
+}
+
+function toRuble(income)
+{
+    return (income * ruble).toFixed(commaValue);
+}
+
+function toRupee(income)
+{
+    return (income * rupee).toFixed(commaValue)
+}
